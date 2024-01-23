e075ea84004267c8441fe06c5cd68fb3228c5859
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2024-01-23 20:40:18 +0000

    Add aspnet health check for silo health

---
+++ Orleans.Utilities.Healthchecks/SiloHealthCheck.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Orleans.Runtime;
+
+namespace Orleans.Utilities.Healthchecks;
+
+public class SiloHealthCheck : IHealthCheck
+{
+    private DateTime lastCheckTime;
+    private readonly IEnumerable<IHealthCheckParticipant> participants;
+
+    public SiloHealthCheck(IEnumerable<IHealthCheckParticipant> participants)
+    {
+        this.participants = participants;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        var lastChecked = this.lastCheckTime;
+        this.lastCheckTime = DateTime.UtcNow;
+        foreach (var participant in this.participants)
+        {
+            if (!participant.CheckHealth(lastChecked, out var reason))
+            {
+                return Task.FromResult(HealthCheckResult.Degraded($"Silo health check participant '{participant.GetType().Name}' returned unhealthy: {reason}"));
+            }
+        }
+
+        return Task.FromResult(HealthCheckResult.Healthy());
+    }
+}
